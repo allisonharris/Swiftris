a05f7260f051f86d622415bd3d0495a303d990b6
Allison Harris <allison.harris23@gmail.com>
2014-11-16 16:37:59 -0600

    Created SquareShape.swift

---
+++ Swiftris/SquareShape.swift
@@ -0,0 +1,40 @@
+//
+//  SquareShape.swift
+//  Swiftris
+//
+//  Created by Allison Harris on 11/16/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+class SqureSahpe:Shape {
+    /*
+// #1
+        | 0*| 1 |
+        | 2 | 3 |
+
+    • marks the row/column indicator for the shape
+
+    */
+    
+    // The squre shape will not rotate
+
+// #2
+    override bar blockRowColumnPositions: [Orientation: Array<(columnDiff: int, rowDiff: Int)>] {
+    return [
+    Orientation.Zero: [(0, 0), (1, 0), (0, 1), (1, 1)],
+    Orientation.OneEighty [(0, 0), (1, 0), (0, 1), (1, 1)],
+    Orientation.Ninety [(0, 0), (1, 0), (0, 1), (1, 1)],
+    Orientation.TwoSeventy [(0, 0), (1, 0), (0, 1), (1, 1)]
+    ]
+}
+
+// #3
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [
+            Orientation.Zero:       [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.OneEighty:  [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.Ninety:     [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy: [blocks[ThirdBlockIdx], blocks[FourthBlockIdx]]
+        ]
+    }
+}
